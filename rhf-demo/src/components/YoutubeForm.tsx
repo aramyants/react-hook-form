42a7126b6287c698405955d733c090293980766d
aramyants <aramaramyants@gmail.com>
2025-08-25 19:13:32 +0400

    chore: Disable Form Submission, fs state, reset, async validation, validation modes and manually trigger

--- rhf-demo/src/components/YoutubeForm.tsx
+++ rhf-demo/src/components/YoutubeForm.tsx
@@ -33,16 +33,33 @@ export const YoutubeForm = () => {
       age: 0,
       dob: new Date(),
     },
+    mode: 'all',
   });
+
   const {
     register,
     handleSubmit,
     control,
-    formState: { errors, touchedFields, dirtyFields, isDirty },
+    formState: {
+      errors,
+      touchedFields,
+      dirtyFields,
+      isDirty,
+      isValid,
+      isSubmitting,
+      isSubmitted,
+      isSubmitSuccessful,
+      submitCount,
+    },
     watch,
     getValues,
     setValue,
+    reset,
+    trigger
   } = form;
+
+  // console.log(isSubmitting, isSubmitted, isSubmitSuccessful, submitCount);
+  // console.log({ touchedFields, dirtyFields, isDirty, isValid });
 
   const onSubmit = (data: FormValues) => {
     console.log('Form submitted', data);
@@ -55,12 +72,22 @@ export const YoutubeForm = () => {
   //   return () => subscription.unsubscribe();
   // }, [watch]);
 
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
+
   const handleGetValues = () => {
     console.log('Form values:', getValues());
   };
 
   const handleSetValue = () => {
-     setValue("username", "Superman", { shouldDirty: true, shouldTouch: true, shouldValidate: true });
+    setValue('username', 'Superman', {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
   };
 
   const { fields, append, remove } = useFieldArray({
@@ -68,12 +95,9 @@ export const YoutubeForm = () => {
     control,
   });
 
-
   const onError = (errors: FieldErrors<FormValues>) => {
     console.log('Form errors:', errors);
   };
-
-  console.log({ touchedFields, dirtyFields, isDirty });
 
   // const watchForm = watch();
 
@@ -102,7 +126,6 @@ export const YoutubeForm = () => {
           />
           <p className="error">{errors.username?.message}</p>
         </div>
-
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input
@@ -127,12 +150,18 @@ export const YoutubeForm = () => {
                 endsWithCom: (value) => {
                   return value.endsWith('.com') || 'Email must end with .com';
                 },
+                emailAvailable: async (value) => {
+                  const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/users?email=${value}`
+                  );
+                  const data = await response.json();
+                  return data.length === 0 || 'Email already exists';
+                },
               },
             })}
           />
           <p className="error">{errors.email?.message}</p>
         </div>
-
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <input
@@ -147,18 +176,20 @@ export const YoutubeForm = () => {
           />
           <p className="error">{errors.channel?.message}</p>
         </div>
-
         <div className="form-control">
           <label htmlFor="twitter">Twitter</label>
-          <input type="text" id="twitter" {...register('social.twitter', {
-            required: {
-              value: true,
-              message: 'Twitter handle is required',
-            },
-            disabled: watch('channel') === '',
-          })} />
+          <input
+            type="text"
+            id="twitter"
+            {...register('social.twitter', {
+              required: {
+                value: true,
+                message: 'Twitter handle is required',
+              },
+              disabled: watch('channel') === '',
+            })}
+          />
         </div>
-
         <div className="form-control">
           <label htmlFor="facebook">Facebook</label>
           <input
@@ -167,7 +198,6 @@ export const YoutubeForm = () => {
             {...register('social.facebook', {})}
           />
         </div>
-
         <div className="form-control">
           <label htmlFor="primary-phone">Primary phone number</label>
           <input
@@ -176,7 +206,6 @@ export const YoutubeForm = () => {
             {...register('phoneNumbers.0', {})}
           />
         </div>
-
         <div className="form-control">
           <label htmlFor="secondary-phone">Secondary phone number</label>
           <input
@@ -185,7 +214,6 @@ export const YoutubeForm = () => {
             {...register('phoneNumbers.1', {})}
           />
         </div>
-
         <div>
           <label>List of phone numbers</label>
           <button
@@ -210,7 +238,6 @@ export const YoutubeForm = () => {
             </div>
           ))}
         </div>
-
         <div className="form-control">
           <label htmlFor="age">Age</label>
           <input
@@ -226,7 +253,6 @@ export const YoutubeForm = () => {
           />
           <p className="error">{errors.age?.message}</p>
         </div>
-
         <div className="form-control">
           <label htmlFor="dob">Date of birth</label>
           <input
@@ -242,13 +268,20 @@ export const YoutubeForm = () => {
           />
           <p className="error">{errors.dob?.message}</p>
         </div>
-
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={!isDirty || !isValid || isSubmitting}>
+          Submit
+        </button>
+        <button type="button" onClick={() => reset()}>
+          Reset
+        </button>{' '}
         <button type="button" onClick={handleGetValues}>
           Get Values
         </button>
         <button type="button" onClick={handleSetValue}>
-          Set Values
+          Set Value
+        </button>
+        <button type="button" onClick={() => trigger("email")}>
+          Validate
         </button>
       </form>
       <DevTool control={control} />
